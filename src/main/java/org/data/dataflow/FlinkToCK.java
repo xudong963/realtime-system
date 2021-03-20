054bfba8d289d00f2a01b6b18588d6f26b3e2069
王旭东 <wangxudong.zsy@bytedance.com>
2021-03-20 19:00:15 +0800

    nearly finish dataflow, blocked by https://stackoverflow.com/questions/66720528/org-apache-http-protocol-httprequestexecutor6ea2bc93-is-not-serializable-the-o

--- src/main/java/org/data/dataflow/FlinkToCK.java
+++ src/main/java/org/data/dataflow/FlinkToCK.java
@@ -1,19 +1,30 @@
 package org.data.dataflow;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 
-import org.apache.flink.streaming.api.functions.source.RichSourceFunction;
+class FlinkToCK extends RichSinkFunction<String> {
 
-class FlinkToCk implements RichSourceFunction<String> {
-
-  @Override
-  public void run(SourceContext<String> sourceContext) throws Exception {
-
+  FlinkToCK(String host, String port, String user, String pwd) throws SQLException {
+    super();
+    this.connection_ = DriverManager.getConnection("jdbc:clickhouse://" + host + ":" + port, user, pwd);
+    this.statement_ = connection_.createStatement();
+    statement_.execute("CREATE DATABASE IF NOT EXISTS test");
   }
 
   @Override
-  public void cancel() {
-
+  public void invoke(String value, SinkFunction.Context context) throws SQLException {
+    statement_.execute("INSERT INTO test VALUES ('${value}')");
   }
 
-  private
+  public void close() throws SQLException {
+    connection_.close();
+  }
+
+  private final Connection connection_;
+  private final Statement statement_;
 }
--- src/main/java/org/data/dataflow/KafkaToFlink.java
+++ src/main/java/org/data/dataflow/KafkaToFlink.java
@@ -4,11 +4,13 @@ package org.data.dataflow;
 import java.util.Properties;
 import java.util.concurrent.ThreadLocalRandom;
 import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.common.serialization.SimpleStringSchema;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.connectors.kafka.KafkaDeserializationSchema;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 // flink consume kafka data
 public class KafkaToFlink {
@@ -20,15 +22,33 @@ public class KafkaToFlink {
     properties.setProperty("bootstrap.servers", "10.227.89.202:9092");
     properties.setProperty("group.id", "test-consumer-group");
     SingleOutputStreamOperator<String> dataStreamSource = streamExecutionEnvironment
-        .addSource(new FlinkKafkaConsumer<String>(
+        .addSource(new FlinkKafkaConsumer<>(
             "my-topic",
-            new SimpleStringSchema(),
+            new CustomKafkaDeserializationSchema(),
             properties).setStartFromEarliest()
         ).map((MapFunction<String, String>) s -> {
           Thread.sleep(ThreadLocalRandom.current().nextInt(0, 500));
           return s;
         });
-    dataStreamSource.addSink(new FlinkToCK());
+    dataStreamSource.addSink(new FlinkToCK("10.227.89.202", 8123 + "", "default", ""));
     streamExecutionEnvironment.execute("flink consume kafka topic");
+  }
+}
+
+class CustomKafkaDeserializationSchema implements KafkaDeserializationSchema<String> {
+
+  @Override
+  public boolean isEndOfStream(String s) {
+    return false;
+  }
+
+  @Override
+  public String deserialize(ConsumerRecord<byte[], byte[]> consumerRecord) throws Exception {
+    return consumerRecord.toString();
+  }
+
+  @Override
+  public TypeInformation<String> getProducedType() {
+    return BasicTypeInfo.STRING_TYPE_INFO;
   }
 }
